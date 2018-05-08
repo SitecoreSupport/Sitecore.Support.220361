f002396332984a7f44f30b8933a5ee062f9c5446
DK\obr <olegbraginskyy@sitecore.net>
2018-05-08 13:55:21 +0300

    If EnableItemLanguageFallback is enabled, then the additional update for the first version of the item (which is present as the fallback version) is performed

---
+++ src/Sitecore.Support.220361/ContentSearch/SitecoreItemCrawler.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.Data.Managers;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch
+{
+  public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
+  {
+    public SitecoreItemCrawler()
+    {
+    }
+    public override void Update(IProviderUpdateContext context, IIndexableUniqueId indexableUniqueId, IndexEntryOperationContext operationContext, IndexingOptions indexingOptions = 0)
+    {
+      base.Update(context, indexableUniqueId, operationContext, indexingOptions);
+      ItemUri itemUri = indexableUniqueId as SitecoreItemUniqueId;
+      ItemUri itemUriFirstVersion = new ItemUri(itemUri.ItemID, itemUri.Language, Data.Version.First, itemUri.DatabaseName);
+      SitecoreItemUniqueId indexableUniqueIdFirstVersion = new SitecoreItemUniqueId(itemUriFirstVersion);
+      if (context.Index.EnableItemLanguageFallback)
+      {
+        base.Update(context, indexableUniqueIdFirstVersion, operationContext, indexingOptions);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.220361/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.220361")]
+[assembly: AssemblyProduct("Sitecore.Support.220361")]
+[assembly: ComVisible(false)]
